3e16a8182c0b59fad1f7d6f2de1fa576b4babd2a
EduardoCarruci <edugocarruci@gmail.com>
2023-08-16 00:52:38 -0400

    QUE ES ESTA MEIRDA

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,6 +1,11 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { LngLat, Map, Marker } from 'mapbox-gl';
 
+interface MarkerData {
+  marker: Marker;
+  color: string;
+}
+
 @Component({
   selector: 'pages-markers-page',
   templateUrl: './markers-page.component.html',
@@ -8,58 +13,42 @@ import { LngLat, Map, Marker } from 'mapbox-gl';
 })
 export class MarkersPageComponent {
 
-  @ViewChild('map')
-
-  public markers: Marker[] = [];
-
-  public divMap?: ElementRef;
-  public map?: Map ;
+  @ViewChild('map') divMap?: ElementRef;
+  public map?: Map;
   public currentCenter: LngLat = new LngLat(-62.760927214894224, 8.25934378195467);
-
+  public markerData: MarkerData[] = [];
 
   ngAfterViewInit(): void {
-
-    if ( !this.divMap ) throw 'El elemento Html no fue encontrado';
+    if (!this.divMap) throw 'El elemento Html no fue encontrado';
 
     this.map = new Map({
-      container: this.divMap.nativeElement, // container ID
-      style: 'mapbox://styles/mapbox/streets-v12', // style URL
-      center: [-62.760927214894224, 8.25934378195467], // starting position [lng, lat]
+      container: this.divMap.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [-62.760927214894224, 8.25934378195467],
       zoom: 13,
-      });
-
-      // const markerHtml = document.createElement('div');
-      // markerHtml.innerHTML = 'Eduardo Valderrama'
-
-      // const marker = new Marker({
-      //   // color: 'red'
-      //   element: markerHtml,
-      // })
-      //   .setLngLat( this.currentCenter )
-      //   .addTo( this.map );
-
+    });
   }
 
   createMarker() {
-    if ( !this.map ) return;
+    if (!this.map) return;
 
-    const color = '#xxxxxx'.replace(/x/g, y=>(Math.random()*16|0).toString(16));
+    const color = '#xxxxxx'.replace(/x/g, y => (Math.random() * 16 | 0).toString(16));
     const lngLat = this.map.getCenter();
 
-
-    this.addMarker( lngLat, color );
+    this.addMarker(lngLat, color);
   }
 
-  addMarker( lngLat: LngLat, color: string ) {
-    if( !this.map ) return;
+  addMarker(lngLat: LngLat, color: string) {
+    if (!this.map) return;
 
     const marker = new Marker({
       color: color,
       draggable: true,
     })
-      .setLngLat( lngLat)
-      .addTo( this.map );
-    this.markers.push( marker );
-  }
+      .setLngLat(lngLat)
+      .addTo(this.map);
 
+    const markerData: MarkerData = { marker, color };
+    this.markerData.push(markerData);
+  }
 }
